0a38b1322705a15e02f8066b82a0f6fc6cce27a4
AkselAntonsen <598909@stud.hvl.no>
2025-02-25 00:34:08 +0100

    3_av_8_tester_ferdig

--- smarthouse/domain.py
+++ smarthouse/domain.py
@@ -1,4 +1,4 @@
-from typing import List, Optional
+from typing import List, Dict, Optional
 from datetime import datetime
 
 
@@ -7,66 +7,54 @@ class Measurement:
     """
     This class represents a measurement taken from a sensor.
     """
-
     def __init__(self, timestamp: datetime, value: float, unit: str):
         self.timestamp = timestamp
         self.value = value
         self.unit = unit
 
-    def __repr__(self):
-        return f"Measurement({self.timestamp}, {self.value} {self.unit})"
+
 
 
 ########################################################
 
+
+
+
+
 class Device:
-    def __init__(self, device_ID: str, name: str):
-        self.device_ID = device_ID
+    def __init__(self, device_id: str, name: str):
+        self.device_id = device_id
         self.name = name
 
-    def __repr__(self):
-        return f"Device(ID={self.device_ID}, Name={self.name})"
+    def is_sensor(self):
+        return isinstance(self, Sensor)
 
-
-class Actuator(Device):
-    def __init__(self, device_ID: str, name: str, state: bool = False):
-        super().__init__(device_ID, name)
-        self.state = state  # ON or OFF
-
-    def toggle(self):
-        self.state = not self.state
-        return self.state
-
-    def __repr__(self):
-        return f"Actuator(ID={self.device_ID}, Name={self.name}, State={'ON' if self.state else 'OFF'})"
+    def is_actuator(self):
+        return isinstance(self, Actuator)
 
 
 class Sensor(Device):
-    def __init__(self, device_ID: str, name: str):
-        super().__init__(device_ID, name)
-        self.measurements: List[Measurement] = []
+    def __init__(self, device_id: str, name: str, measurements: Optional[List[Measurement]] = None):
+        super().__init__(device_id, name)
+        self.measurements = measurements or []
 
-    def add_measurement(self, value: float, unit: str):
-        measurement = Measurement(datetime.now(), value, unit)
-        self.measurements.append(measurement)
-
-    def get_measurements(self):
-        return self.measurements
-
-    def __repr__(self):
-        return f"Sensor(ID={self.device_ID}, Name={self.name}, Measurements={self.measurements})"
+    def last_measurement(self):
+        return self.measurements[-1] if self.measurements else None
 
 
-class HybridDevice(Sensor, Actuator):
+class Actuator(Device):
+    def __init__(self, device_id: str, name: str, state: bool = False):
+        super().__init__(device_id, name)
+        self.state = state  # ON or OFF
 
-    # Combines functions from Sensor and Actuator
+    def turn_on(self, value: Optional[float] = None):
+        self.state = True
 
-    def __init__(self, device_ID: str, name: str):
-        Sensor.__init__(self, device_ID, name)
-        Actuator.__init__(self, device_ID, name, state=False)
+    def turn_off(self):
+        self.state = False
 
-    def __repr__(self):
-        return f"HybridDevice(ID={self.device_ID}, Name={self.name}, State={'ON' if self.state else 'OFF'}, Measurements={self.measurements})"
+    def is_active(self):
+        return self.state
 
 
 class Room:
@@ -78,12 +66,8 @@ class Room:
     def add_device(self, device: Device):
         # Registers a new device in the room
         self.devices.append(device)
+        device.room = self
 
-    def get_device(self):
-        return self.devices
-
-    def __repr__(self):
-        return f"Room(Name={self.name}, Area={self.area}, Devices={self.devices})"
 
 class Floor:
     def __init__(self, level: int):
@@ -92,12 +76,6 @@ class Floor:
 
     def add_room(self, room: Room):
         self.rooms.append(room)
-
-    def get_rooms(self):
-        return self.rooms
-
-    def __repr__(self):
-        return f"Floor(Level={self.level}, Rooms={self.rooms})"
 
 
 ########################################################
@@ -114,47 +92,42 @@ class SmartHouse:
     """
     def __init__(self):
         self.floors: List[Floor] = []
+        self.devices: Dict[str, Device] = {}
+
 
     def register_floor(self, level: int) -> Floor:
         """
         This method registers a new floor at the given level in the house
         and returns the respective floor object.
         """
-        for floor in self.floors:
-            if floor.level == level:
-                return floor
-
-        new_floor = Floor(level)
-        self.floors.append(new_floor)
-        self.floors.sort(key=lambda x: x.level)
-        return new_floor
+        floor = Floor(level)
+        self.floors.append(floor)
+        return floor
 
 
     def register_room(self, floor: Floor, room_size: float, room_name: Optional[str] = None) -> Room:
         """
-        This methods registers a new room with the given room areal size 
+        These methods registers a new room with the given room areal size
         at the given floor. Optionally the room may be assigned a mnemonic name.
         """
-        if not room_name:
-            room_name = f"Room {len(floor.rooms) + 1}"
-        new_room = Room(room_name, room_size)
-        floor.add_room(new_room)
-        return new_room
+        room = Room(room_name, room_size)
+        floor.add_room(room)
+        return room
 
 
     def get_floors(self) -> List[Floor]:
         """
         This method returns the list of registered floors in the house.
         The list is ordered by the floor levels, e.g. if the house has 
-        registered a basement (level=0), a ground floor (level=1) and a first floor 
-        (leve=1), then the resulting list contains these three flors in the above order.
+        registered a basement (level=0), a ground-floor (level=1) and a first floor
+        (level=1), then the resulting list contains these three floors in the above order.
         """
-        return self.floors
+        return sorted(self.floors, key=lambda f: f.level)
 
 
     def get_rooms(self) -> List[Room]:
         """
-        This methods returns the list of all registered rooms in the house.
+        These methods return the list of all registered rooms in the house.
         The resulting list has no particular order.
         """
         return [room for floor in self.floors for room in floor.rooms]
@@ -162,28 +135,29 @@ class SmartHouse:
 
     def get_area(self) -> float:
         """
-        This methods return the total area size of the house, i.e. the sum of the area sizes of each room in the house.
+        These methods return the total area size of the house, i.e. the sum of the area sizes of each room in the house.
         """
         return sum(room.area for room in self.get_rooms())
 
+
     def register_device(self, room: Room, device: Device):
         """
-        This methods registers a given device in a given room.
+        These methods register a given device in a given room.
         """
         room.add_device(device)
+        self.devices[device.device_id] = device
 
     
     def get_device(self, device_id: str) -> Optional[Device]:
         """
         This method retrieves a device object via its id.
         """
-        for room in self.get_rooms():
-            for device in room.get_devices():
-                if device.device_ID == device_id:
-                    return device
-        return None
+        return self.devices.get(device_id)
 
 
-    def __repr__(self):
-        return f"SmartHouse(Floors={self.floors})"
+    def get_devices(self) -> List[Device]:
+        return list(self.devices.values())
+
+
+
 
--- tests/demo_house.py
+++ tests/demo_house.py
@@ -4,15 +4,15 @@ from datetime import datetime
 DEMO_HOUSE = SmartHouse()
 
 
-# Registrere floors
+# Register floors
 
 floor1 = DEMO_HOUSE.register_floor(1)
 floor2 = DEMO_HOUSE.register_floor(2)
 
-# Registrere rooms:
+# Register rooms:
 
 entrance = DEMO_HOUSE.register_room(floor1, 13.5, "Entrance")
-living_room_kitchen = DEMO_HOUSE.register_room(floor1, 39.75, "LivingRoom/Kitchen")
+living_room_kitchen = DEMO_HOUSE.register_room(floor1, 39.75, "Living room/Kitchen")
 bathroom1 = DEMO_HOUSE.register_room(floor1, 6.3, "Bathroom 1")
 guestroom1 = DEMO_HOUSE.register_room(floor1, 8.0, "Guest Room 1")
 garage = DEMO_HOUSE.register_room(floor1, 19.0, "Garage")
@@ -23,14 +23,47 @@ guestroom2 = DEMO_HOUSE.register_room(floor2, 8.0, "Guest Room 2")
 hallway = DEMO_HOUSE.register_room(floor2, 10.0, "Hallway")
 guestroom3 = DEMO_HOUSE.register_room(floor2, 10.0, "Guest Room 3")
 dressingroom = DEMO_HOUSE.register_room(floor2, 4.0, "Dressing Room")
-masterbedroom = DEMO_HOUSE.register_room(floor2, 7.0, "Master Bedrom")
+masterbedroom = DEMO_HOUSE.register_room(floor2, 17.0, "Master Bedroom")
 
 
-# Registrere devices
+# Register devices
+
+sensor1 = Sensor("3d87e5c0-8716-4b0b-9c67-087eaaed7b45", "Humidity Sensor")
+sensor2 = Sensor("cd5be4e8-0e6b-4cb5-a21f-819d06cf5fc5", "Motion Sensor")
+sensor3 = Sensor("a2f8690f-2b3a-43cd-90b8-9deea98b42a7", "Electricity Meter")
+sensor4 = Sensor("8a43b2d7-e8d3-4f3d-b832-7dbf37bf629e", "CO2 sensor")
+sensor5 = Sensor("7c6e35e1-2d8b-4d81-a586-5d01a03bb02c", "Air Quality Sensor")
+sensor6 = Sensor("4d8b1d62-7921-4917-9b70-bbd31f6e2e8e", "Temperature Sensor", [Measurement(datetime.now(), 22.5, "°C")])
+
+actuator1 = Actuator("8d4e4c98-21a9-4d1e-bf18-523285ad90f6", "Smart Oven", True)
+actuator2 = Actuator("9a54c1ec-0cb5-45a7-b20d-2a7349f1b132", "Automatic Garage Door", False)
+actuator3 = Actuator("5e13cabc-5c58-4bb3-82a2-3039e4480a6d", "Heat Pump", True)
+actuator4 = Actuator("4d5f1ac6-906a-4fd1-b4bf-3a0671e4c4f1", "Smart Lock", True)
+actuator5 = Actuator("1a66c3d6-22b2-446e-bf5c-eb5b9d1a8c79", "Smart PLug", False)
+actuator6 = Actuator("9e5b8274-4e77-4e4e-80d2-b40d648ea02a", "Dehumidifier", True)
+actuator7 = Actuator("6b1c5f6b-37f6-4e3d-9145-1cfbe2f1fc28", "Light Bulp", True)
+actuator8 = Actuator("c1e8fa9c-4b8d-487a-a1a5-2b148ee9d2d1", "Smart Oven", False)
 
 
-sensor1 = Sensor("4d8b1d62-7921-4917-9b70-bbd31f6e2e8e", "Temperature Sensor", [Measurement(datetime.now(), 22.5, "°C")])
+# Assigning devices to rooms
+
+DEMO_HOUSE.register_device(living_room_kitchen, sensor2)
+DEMO_HOUSE.register_device(living_room_kitchen, sensor4)
+DEMO_HOUSE.register_device(living_room_kitchen, actuator3)
+DEMO_HOUSE.register_device(entrance, actuator4)
+DEMO_HOUSE.register_device(entrance, sensor3)
+DEMO_HOUSE.register_device(bathroom1, sensor1)
+DEMO_HOUSE.register_device(guestroom1, actuator1)
+DEMO_HOUSE.register_device(garage, actuator2)
+
+DEMO_HOUSE.register_device(office, actuator5)
+DEMO_HOUSE.register_device(bathroom2, actuator6)
+DEMO_HOUSE.register_device(guestroom2, actuator7)
+DEMO_HOUSE.register_device(guestroom3, sensor5)
+DEMO_HOUSE.register_device(masterbedroom, sensor6)
+DEMO_HOUSE.register_device(masterbedroom, actuator8)
 
 
-# TODO: continue registering the remaining floor, rooms and devices
+
+
 
